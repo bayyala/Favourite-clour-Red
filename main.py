c1e249fd86639df016a6e808c7c658ca101ac266
Bala Ayyala <122984990+bayyala@users.noreply.github.com>
2023-03-19 14:34:59 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+#Favourite colour RED
+
+colour=input("Enter your favourite colour: ")
+if colour== "RED" or colour=="Red" or colour=="red":
+   print("I like red too")
+else:
+  print("I dont like ",[colour],"I prefer Red")
